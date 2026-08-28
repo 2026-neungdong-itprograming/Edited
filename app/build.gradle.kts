05b5b7b753f3783840bc55ca2ab76fd922d36f54
Junwon Yoon <me@parin.asia>
2026-08-28 16:28:42 +0900

    Add Depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,16 @@ plugins {
 }
 
 dependencies {
-    // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
-    implementation(project(":utils"))
+    // Source: https://mvnrepository.com/artifact/it.unimi.dsi/fastutil
+    implementation("it.unimi.dsi:fastutil:8.5.19")
+    // Source: https://mvnrepository.com/artifact/org.jctools/jctools-core
+    implementation("org.jctools:jctools-core:4.0.7")
+    // Source: https://mvnrepository.com/artifact/net.java.dev.jna/jna
+    implementation("net.java.dev.jna:jna:5.19.1")
+    // Source: https://mvnrepository.com/artifact/org.apache.lucene/lucene-analyzers-nori
+    implementation("org.apache.lucene:lucene-analyzers-nori:8.11.4")
+    // Source: https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
+    implementation("org.eclipse.jgit:org.eclipse.jgit:7.7.1.202607240634-r")
 }
 
 application {
